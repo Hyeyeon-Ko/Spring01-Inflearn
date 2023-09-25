01c04b43bf63b8a1037c3b8db6e1debb828e6f89
Hyeyeon-Ko <khy33355@gmail.com>
2023-09-26 00:28:42 +0900

    컴포넌트 스캔

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+// Spring bean을 전체를 scan하기 위해 @ComponentScan를 등록
+@ComponentScan(
+        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
