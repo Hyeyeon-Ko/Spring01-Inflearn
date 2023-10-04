37c67ecb6e2e8b5632ce86ad314502350e01ad6f
Hyeyeon-Ko <khy33355@gmail.com>
2023-10-04 17:03:00 +0900

    조회한 빈이 모두 필요할 때 List, Map

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 // Spring bean을 전체를 scan하기 위해 @ComponentScan를 등록
 @ComponentScan(
-        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
